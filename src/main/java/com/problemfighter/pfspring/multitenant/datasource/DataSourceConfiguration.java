42f6b22f54af4cb11233180ba35a7b5212228403
Touhid Mia <hmtm.cse@gmail.com>
2020-09-07 15:34:20 +0600

    Multi Resource Register

--- src/main/java/com/problemfighter/pfspring/multitenant/datasource/DataSourceConfiguration.java
+++ src/main/java/com/problemfighter/pfspring/multitenant/datasource/DataSourceConfiguration.java
@@ -5,7 +5,6 @@ import com.problemfighter.pfspring.multitenant.datasource.processor.ProcessConfi
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import javax.sql.DataSource;
 import java.util.Map;
 
