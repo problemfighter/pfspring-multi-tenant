7cc0bc9073865b305b01b8ef2f1ec568b4d9deda
Touhid Mia <hmtm.cse@gmail.com>
2020-09-07 15:33:22 +0600

    Multi Resource Register

--- src/main/java/com/problemfighter/pfspring/multitenant/datasource/DataSourceConfiguration.java
+++ src/main/java/com/problemfighter/pfspring/multitenant/datasource/DataSourceConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.Map;
 
 @Configuration
 public class DataSourceConfiguration {
@@ -14,15 +15,22 @@ public class DataSourceConfiguration {
     @Autowired
     private ProcessConfiguration processConfiguration;
 
+    private Map<Object, Object> registeredDataSources;
+
     @Bean
     public DataSource dataSource() {
         if (processConfiguration.isMultiDatasourceEnable()) {
             MultiDatasourceRouter multiDatasourceRouter = new MultiDatasourceRouter();
             multiDatasourceRouter.setDefaultTargetDataSource(processConfiguration.getDefaultDatasource());
-            multiDatasourceRouter.setTargetDataSources(processConfiguration.getAllRegisteredDatasource());
+            registeredDataSources = processConfiguration.getAllRegisteredDatasource();
+            multiDatasourceRouter.setTargetDataSources(registeredDataSources);
             return multiDatasourceRouter;
         } else {
             return processConfiguration.getDefaultDatasource();
         }
+    }
+
+    public void addDataSource(String key, Object source){
+        registeredDataSources.put(key, source);
     }
 }
--- src/main/java/com/problemfighter/pfspring/multitenant/datasource/processor/ProcessConfiguration.java
+++ src/main/java/com/problemfighter/pfspring/multitenant/datasource/processor/ProcessConfiguration.java
@@ -18,9 +18,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 @Component
