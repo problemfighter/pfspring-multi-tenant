eac535dbc1ce3a0f7dbeac3f0cc9bbb0ef35fb6e
Touhid Mia <hmtm.cse@gmail.com>
2020-09-07 09:22:28 +0600

    Multi Source Id Resolver

--- src/main/java/com/problemfighter/pfspring/multitenant/datasource/processor/DatasourceIdentityProcessor.java
+++ src/main/java/com/problemfighter/pfspring/multitenant/datasource/processor/DatasourceIdentityProcessor.java
@@ -3,19 +3,28 @@ package com.problemfighter.pfspring.multitenant.datasource.processor;
 import com.problemfighter.pfspring.multitenant.common.MTConstant;
 import com.problemfighter.pfspring.multitenant.common.MultiDatasourceException;
 import com.problemfighter.pfspring.multitenant.datasource.config.MultiDatabaseConfig;
+import com.problemfighter.pfspring.multitenant.datasource.config.UrlMapToDatabase;
 import com.problemfighter.pfspring.multitenant.datasource.data.DatasourceProperty;
 import com.problemfighter.pfspring.multitenant.datasource.data.InstanceInfo;
 import com.problemfighter.pfspring.multitenant.datasource.holder.DatabaseIdentifierHolder;
 import com.problemfighter.pfspring.multitenant.model.entity.InstanceStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class DatasourceIdentityProcessor {
 
     @Autowired
     private MultiDatabaseConfig multiDatabaseConfig;
+
+    @Autowired
+    private UrlMapToDatabase urlMapToDatabase;
 
     private String getDbKeyFromRequest(WebRequest request) {
         return request.getHeader(multiDatabaseConfig.dbIdentifierKey);
@@ -28,7 +37,39 @@ public class DatasourceIdentityProcessor {
         }
     }
 
+    private String getRequestUrl(WebRequest request) {
+        return ((ServletWebRequest) request).getRequest().getRequestURI();
+    }
+
+    private Boolean isRegexMatch(String patternString, String matcherString) {
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(matcherString);
+        return matcher.find();
+    }
+
+    private String regexUrlMappingCheck(String url) {
+        for (Map.Entry<String, String> entry : urlMapToDatabase.regexMapping.entrySet()) {
+            if (isRegexMatch(entry.getKey(), url)) {
+                return entry.getValue();
+            }
+        }
+        return null;
+    }
+
     private String urlToDatabaseKey(String databaseKey, WebRequest request) {
+        if (urlMapToDatabase.regularMapping.size() == 0 && urlMapToDatabase.regexMapping.size() == 0) {
+            return databaseKey;
+        }
+
+        String url = getRequestUrl(request);
+        if (urlMapToDatabase.regularMapping.get(url) != null) {
+            return urlMapToDatabase.regularMapping.get(url);
+        } else {
+            String regexMatchedUrl = regexUrlMappingCheck(url);
+            if (regexMatchedUrl != null) {
+                return regexMatchedUrl;
+            }
+        }
         return databaseKey;
     }
 
@@ -36,6 +77,7 @@ public class DatasourceIdentityProcessor {
         if (!multiDatabaseConfig.enable) {
             return MTConstant.defaultKey;
         }
+
         String databaseKey = getDbKeyFromRequest(request);
         if (databaseKey != null && !DatabaseIdentifierHolder.isRegistered(databaseKey)) {
             throw new MultiDatasourceException(MTConstant.NOT_FOUND).setStatus(MTConstant.NOT_FOUND);
